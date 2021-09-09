18b9cdcd0e75af8b3d8fac87795ae702278e9e57
Ethan Ford <ethan.ford06@gmail.com>
2021-09-08 21:03:39 -0500

    Completed download progress display

--- Zerolingo.net/DownloadController.cs
+++ Zerolingo.net/DownloadController.cs
@@ -9,22 +9,20 @@ namespace Zerolingo
     static class DownloadController 
     {
         public static async Task DownloadDefaultAsync() {
-            Console.WriteLine($"Downloading Chromium/{BrowserFetcher.DefaultRevision}");
+            Console.WriteLine($"Downloading Chromium/{BrowserFetcher.DefaultRevision}:");
             BrowserFetcher fetcher = new BrowserFetcher();
 
-            await fetcher.DownloadAsync(BrowserFetcher.DefaultRevision);
-
             fetcher.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DisplayDownloadProgress);
+
+            await fetcher.DownloadAsync(BrowserFetcher.DefaultRevision);
         }
-        static void DisplayDownloadProgress(object sender,DownloadProgressChangedEventArgs e) {
-            Console.WriteLine(e);
-            
-            
-            // Console.WriteLine("{0}    downloaded {1} of {2} bytes. {3} % complete...",
-            // (string)e.UserState,
-            // e.BytesReceived,
-            // e.TotalBytesToReceive,
-            // e.ProgressPercentage);
+        static void DisplayDownloadProgress(object sender, DownloadProgressChangedEventArgs e) {
+            // Console.WriteLine(e);
+
+            Console.Write("\rDownloaded {0} of {1} bytes. {2} % complete...",
+            e.BytesReceived,
+            e.TotalBytesToReceive,
+            e.ProgressPercentage);
         }
     }
 }
