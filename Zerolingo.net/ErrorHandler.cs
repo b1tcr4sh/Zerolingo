062ea9b7bd4dd3d0d7b89a6f0f025830d6691c48
Ethan Ford <ethan.ford06@gmail.com>
2021-10-06 12:56:13 -0500

    Added some error handling functionality

--- Zerolingo.net/ErrorHandler.cs
+++ Zerolingo.net/ErrorHandler.cs
@@ -6,6 +6,11 @@ namespace Zerolingo {
     public class ErrorHandler {
         Browser browserMember;
         Page pageMember;
+
+        public delegate void ApplicationExitTriggerEventHandler(object sender, ApplicationExitTriggerEventArgs e);
+
+        public event ApplicationExitTriggerEventHandler ApplicationExitTrigger; 
+
         public void RegisterEvents(Browser browser, Page page) {
             browserMember = browser;
             pageMember = page;
@@ -16,14 +21,18 @@ namespace Zerolingo {
             page.Error += new EventHandler<ErrorEventArgs>(PageError);
             page.PageError += new EventHandler<PageErrorEventArgs>(PageException);
             page.RequestFailed += new EventHandler<RequestEventArgs>(ConnectionError);
+
+            ApplicationExitTrigger += new ApplicationExitTriggerEventHandler(ExitApplication);
         }
         public async void BrowserDisconnected(Object sender, EventArgs e) {
-            Console.WriteLine("Chromium instance was disconnected or closed.  This likely means that it crashed, if this was intended, please try running the program again.");
+            Console.WriteLine("Chromium instance was disconnected or closed.  This likely means that it crashed, if this was unintended, please try running the program again.");
 
             if (browserMember.Process != null) {
                 Console.WriteLine("Disposing Chromium Process...");
                 await browserMember.DisposeAsync();
+                
             }
+            ApplicationExitTrigger.Invoke(this, new ApplicationExitTriggerEventArgs(-1));
         }
         public async void PageError(object sender, ErrorEventArgs e) {
             Console.WriteLine("The tab encountered an error: {0}", e.Error);
@@ -31,12 +40,23 @@ namespace Zerolingo {
             await pageMember.DisposeAsync();
         }
         public async void ConnectionError(object sender, RequestEventArgs e) {
-            Console.WriteLine("A request sent by the page failed to connect: {0}", e.Request.ToString());
+            // Console.WriteLine("A request sent by the page failed to connect: {0}: {1}", e.Request.Response.Status, e.Request.Response.StatusText);
 
             await pageMember.DisposeAsync();
         }
         public void PageException(object sender, PageErrorEventArgs e) {
             Console.WriteLine("The current page has encountered an exception: {0}.  Not terminating process...", e.Message);
+        }
+        public void ExitApplication(object sender, ApplicationExitTriggerEventArgs e) {
+            Console.WriteLine("Exiting Process...");
+            
+            Environment.Exit(e.ExitCode);
+        }
+
+
+        public class ApplicationExitTriggerEventArgs {
+            public ApplicationExitTriggerEventArgs(int exitCode) { exitCode = ExitCode; }
+            public int ExitCode { get; }
         }
     }
 }
